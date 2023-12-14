0a5daa90eb3c1c1f0137875bddc69a9c4ea46a08
absim29 <simon.bianca29@gmail.com>
2023-12-14 12:47:08 +0100

    Delete post JS

---
+++ scripts/deletePost.js
@@ -0,0 +1,23 @@
+import { POSTS_API_URL } from "./constants.js";
+import { fetchData } from "./fetchData.js";
+import { singlePostId } from "./utils/getSinglePost.js";
+
+const deleteButton = document.querySelector('#delete-button');
+
+
+async function deletePost() {
+    const postId = singlePostId();
+    const response = await fetchData(`${POSTS_API_URL}/${postId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-type': 'application/json'
+        },
+    }, true);
+    console.log(response);
+
+}
+
+deleteButton.addEventListener('click', async (event) => {
+    event.preventDefault();
+    deletePost();
+});
